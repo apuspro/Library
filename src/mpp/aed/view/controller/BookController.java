be5b09e985e524bb628e7c898cfb2a9eb3a70484
andres <andres@172.17.5.205>
2015-12-01 16:49:00 -0600

    add book and author

---
+++ src/mpp/aed/view/controller/BookController.java
@@ -0,0 +1,55 @@
+package mpp.aed.view.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import mpp.aed.application.Main;
+import mpp.aed.library.Book;
+
+public class BookController {
+
+	@FXML
+	private int ISBN;
+	@FXML
+	private String title;
+	@FXML
+	private int maxChecoutLength;
+	@FXML
+	private int numberOfCopies;
+	
+	private Book book;
+	
+	public void saveBook(){
+		book = new Book(this.title, this.ISBN, this.maxChecoutLength);
+	}
+	
+	@FXML
+	public void openAuthorView(){
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/AuthorView.fxml"));
+		BorderPane page;
+		try {
+			page = (BorderPane) loader.load();
+			Stage authorStage = new Stage();
+			authorStage.setTitle("Library Menu");
+			authorStage.initModality(Modality.WINDOW_MODAL);
+			//authorStage.initOwner(this.); TODO
+			
+			//AuthorController controller = loader.getController();
+			
+			Scene scene = new Scene(page);
+			authorStage.setScene(scene);
+			
+			authorStage.showAndWait();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
