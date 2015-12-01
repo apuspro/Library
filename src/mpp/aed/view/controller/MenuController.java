db33a5c2dc150c2a8e21be3b1dc7168d86b4b351
andres <andres@172.17.5.205>
2015-12-01 14:30:22 -0600

    afloaiza - just added the tag FXML and the call to open bookview

--- src/mpp/aed/view/controller/MenuController.java
+++ src/mpp/aed/view/controller/MenuController.java
@@ -2,6 +2,7 @@ package mpp.aed.view.controller;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -14,9 +15,33 @@ public class MenuController {
 	private Stage primaryStage;
 	private Stage menuStage;
 	
+	@FXML
 	public void openAbout(){
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("../view/AboutView.fxml"));
+		BorderPane page;
+		try {
+			page = (BorderPane) loader.load();
+		
+			Stage aboutStage = new Stage();
+			aboutStage.setTitle("About");
+			aboutStage.initModality(Modality.WINDOW_MODAL);
+			aboutStage.initOwner(this.menuStage);
+			Scene scene = new Scene(page);
+			aboutStage.setScene(scene);
+	
+			// Show the dialog and wait until the user closes it
+			aboutStage.showAndWait();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void openAddBook(){
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("../view/BookView.fxml"));
 		BorderPane page;
 		try {
 			page = (BorderPane) loader.load();
